943d8a7533c9f4d6f902de34245cdbf67d5b9e0b
Julian Alexander Murillo <julian.alexander.murillo@gmail.com>
2023-06-01 09:22:36 -0500

    App application

--- src/index.js
+++ src/index.js
@@ -1,36 +1,49 @@
 #!/usr/bin/env node
-import clear from "clear";
+import yargs from "yargs/yargs";
+// import clear from "clear";
 import chalk from "chalk";
-import figlet from "figlet";
+// import figlet from "figlet";
 import path from "path";
-import { exec } from "child_process";
-import {
-  askAppName,
-  askAndroidStudioInstallation,
-  askIonicInstallation,
-} from "./inputs.js";
+// import { exec } from "child_process";
+import fs from "fs";
 
-clear();
-console.log(chalk.green(figlet.textSync("MAPA")));
-console.log(chalk.bold.yellow("Mobile Application Project Assistant"));
+const Value = (value) => ({
+  is: (secondValue) => value === secondValue,
+  isNot: (secondValue) => value !== secondValue,
+  isGreatherThan: (secondValue) => value > secondValue,
+  isLowerThan: (secondValue) => value < secondValue,
+});
 
-const run = async () => {
-  const { appName, appLocation } = await askAppName();
-  const { isASInstalled } = await askAndroidStudioInstallation();
-  if (!isASInstalled) {
-    await exec("open https://developer.android.com/studio");
+const getApps = () => {
+  const dataFile = fs.readFileSync("./data/data.json", "utf-8")
+  const parsedData = JSON.parse(dataFile);
+  return parsedData.apps;
+};
+
+const runListCommand = (command) => {
+  const apps = getApps();
+  console.log(`${chalk.blue("==>")} ${chalk.bold.white("Apps:")}\n`);
+  apps.forEach((item) => {
+    console.log(
+      `${chalk.yellow("*")} ${chalk.bold.green(item.name)}: ${item.description}`
+    );
+  });
+
+  console.log("");
+};
+
+const run = () => {
+  const command = process.argv[2];
+  const apps = getApps();
+
+  if (Value(command).is("list")) {
+    runListCommand(command);
+    return;
   }
-  const { isIonicInstalled } = await askIonicInstallation();
 
-  if (!isIonicInstalled) {
-    await exec("yarn global add @ionic/cli");
+  if (Value(command).is("add")) {
   }
-
-  if (appLocation) {
-    await exec(`git clone git@github.com:64lines/PlusOne.git "${path.join(appLocation, appName)}"`)
-    console.log(chalk.bold.green("Project Created!"));
-  }
-  console.log(appLocation)
+  // console.log();
 };
 
 run();
